5383594832a5332b17cf5a6e740be363c3d54a2b
corsomk312 <corso2987@gmail.com>
2016-05-28 12:46:53 -0500

    Complete JS Constructor exercise

--- JS102-master/scripts.js
+++ JS102-master/scripts.js
@@ -1,18 +1,60 @@
 //Use this file to implement Part One of your project
 
-var animal = {}
+var tapeWorm = {}
 
-animal.username = 'Daffy'
-animal['tagline'] = 'What\'s up doc?'
+tapeWorm.username = 'Daffy'
+tapeWorm['tagline'] = 'What\'s up doc?'
 var noises = []
-animal['noises'] = noises
-var count = 0
+tapeWorm['noises'] = noises
+// var count = 0
 
-for (var key in animal){
-  count++;
-  if (animal[key] === animal.username) {
-    console.log("Hi my name is " + animal.username);
-  } else if (animal[key] === animal.tagline) {
-    console.log("HI my tagline is " + animal.tagline)
-  }
+// for (var key in tapeWorm){
+//   count++;
+//   if (tapeWorm[key] === tapeWorm.username) {
+//     console.log("Hi my name is " + tapeWorm.username);
+//   } else if (tapeWorm[key] === tapeWorm.tagline) {
+//     console.log("HI my tagline is " + tapeWorm.tagline)
+//   }
+// }
+
+noiseArray = []
+noiseArray[0] = 'Mooo baby'
+noiseArray.push('Quack Quack motha lover')
+noiseArray.unshift('cluck cluck please')
+tapeWorm['noises'] = noiseArray
+
+animals = []
+animals.push(tapeWorm)
+var quackers = animals
+animals.unshift(quackers)
+
+var chicken = {
+  username: 'Charlie',
+  tagline: 'don\'t even thingabootit',
+  noises: ['chicka chicka', 'who you lookin at?', 'tell me about your family']
 }
+
+var rattleSnake = {
+  username: 'Chucky',
+  tagline: 'Who do you think you are?',
+  noises: ['Whats poppin?', 'Where am i?', 'tell me about your dreams']
+}
+
+animals.push(chicken, rattleSnake)
+
+function AnimalMaker(name){
+  return {
+    speak: function(){
+      console.log('Howdy, my name is', name)
+    },
+    name: name,
+    owner: 'Kevin'
+  }
+};
+
+var farm = []
+var animalNames = ['susie', 'stevie', 'scarlett']
+
+for (var i =0; i < animalNames.length; i++){
+  farm.push(AnimalMaker(animalNames[i]))
+};
