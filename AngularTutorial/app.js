b14c74a2ea64d4e86e4985c10f7a2a82ca8a1ef7
corsomk312 <corso2987@gmail.com>
2016-08-30 11:46:01 -0500

    Add basic Angular layout to display an ordered list

---
+++ AngularTutorial/app.js
@@ -0,0 +1,17 @@
+angular.module('flapperNews',[])
+.controller('MainCtrl',[
+  '$scope',
+  function($scope){
+    $scope.test = 'Hello worlds!';
+    $scope.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 6},
+      {title: 'post 3', upvotes: 14},
+      {title: 'post 4', upvotes: 2},
+      {title: 'post 5', upvotes: 6}
+    ]
+
+    $scope.addPost = function(){
+
+    }
+  }]);
