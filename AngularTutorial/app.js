3ca903b11e128897f8f741c5a7ee5eb286e91524
corsomk312 <corso2987@gmail.com>
2016-08-30 12:01:30 -0500

    Enable user adding a post - no empty title

--- AngularTutorial/app.js
+++ AngularTutorial/app.js
@@ -12,6 +12,10 @@ angular.module('flapperNews',[])
     ]
 
     $scope.addPost = function(){
-
+      if(!$scope.title || $scope.title === ''){
+        return;
+      }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title= '';
     }
   }]);
